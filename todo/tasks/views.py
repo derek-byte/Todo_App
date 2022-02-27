cb565e100c92cc283e26e0f12cc9987e5d029eab
derek-byte <derek.s.prog@gmail.com>
2022-02-26 23:51:32 -0500

    Add database and creating todo lists using admin

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
 # Create your views here.
 
 def index(request):
     # # Use HTTP to print something
     # return HttpResponse('Hello World')
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+
+    context = {'tasks' : tasks}
+
+    return render(request, 'tasks/list.html', context)
